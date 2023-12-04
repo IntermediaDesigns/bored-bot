5e8108235c4fded902c39c86a68ae230450c723e
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-03 20:50:20 -0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function Home () {
     fetch('https://apis.scrimba.com/bored/api/activity')
       .then(response => response.json())
       .then(data => {
-        setActivity(data.activity)
+        setActivity(data)
       })
   }
 
